e4e517a73e8d686a58eee6dd8e139063542af0b2
jhddt <1876990651@qq.com>
2025-10-21 15:15:03 +0800

    config本地初始化完成

---
+++ ConfigServerLocal/src/test/java/com/baorant/configserverlocal/ConfigServerLocalApplicationTests.java
@@ -0,0 +1,13 @@
+package com.baorant.configserverlocal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConfigServerLocalApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
