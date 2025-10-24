af5807d83232b1e4ca39218e24469adc03102304
jhddt <1876990651@qq.com>
2025-10-24 09:49:47 +0800

    Redis消息队列项目完成

--- ConfigServerGit/src/main/java/com/baorant/configserverlocal/ConfigServerGitApplication.java
+++ ConfigServerGit/src/main/java/com/baorant/configserverlocal/ConfigServerGitApplication.java
@@ -4,6 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.config.server.EnableConfigServer;
 
+/**
+ * Git配置服务器应用启动类
+ * 该应用作为Spring Cloud Config Server，从Git仓库获取配置文件并提供配置管理功能
+ */
 @EnableConfigServer
 @SpringBootApplication
 public class ConfigServerGitApplication {
--- ConfigServerLocal/src/main/java/com/baorant/configserverlocal/ConfigServerLocalApplication.java
+++ ConfigServerLocal/src/main/java/com/baorant/configserverlocal/ConfigServerLocalApplication.java
@@ -4,11 +4,19 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.config.server.EnableConfigServer;
 
+/**
+ * 本地配置服务器应用启动类
+ * 该应用作为Spring Cloud Config Server，提供配置文件的集中管理功能
+ */
 @EnableConfigServer
 @SpringBootApplication
 public class ConfigServerLocalApplication {
+    /**
+     * 应用程序入口点
+     *
+     * @param args 命令行参数
+     */
     public static void main(String[] args) {
         SpringApplication.run(ConfigServerLocalApplication.class, args);
     }
-
 }
